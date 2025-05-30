e2c6ba23b8b45237e7965e9c7063705ce11ac81d
Honey15 <137152187+Aniruddhan15@users.noreply.github.com>
2025-05-30 12:00:51 +0530

    Update encapsulation.py

--- encapsulation.py
+++ encapsulation.py
@@ -34,3 +34,55 @@ print(acc.deposit(5000))
 print(acc.withdraw(4000))
 print(acc.withdraw(15000))
 print(acc.show_balance())
+
+
+#2 example
+
+class Employee:
+    def __init__(self, name, salary, dept):
+        self.name = name          # public
+        self._department = dept   # protected
+        self.__salary = salary    # private
+
+    def show_salary(self):
+        return self.__salary
+
+class Manager(Employee):
+    def __init__(self, name, salary, dept, team_size):
+        super().__init__(name, salary, dept)
+        self.team_size = team_size  # public
+
+    def show_details(self):
+        print(f"Name: {self.name}")                        # public
+        print(f"Department: {self._department}")           # protected
+        print(f"Salary: {self.show_salary()}")             # private (via method)
+        print(f"Team Size: {self.team_size}")
+
+mgr = Manager("Aniruddhan", 85000, "AI Research", 5)
+mgr.show_details()
+
+
+#3
+class Employee:
+    def __init__(self, name, salary, dept):
+        self.name = name          # public
+        self._department = dept   # protected
+        self.__salary = salary    # private
+
+    def show_salary(self):
+        return self.__salary
+
+class Manager(Employee):
+    def __init__(self, name, salary, dept, team_size):
+        super().__init__(name, salary, dept)
+        self.team_size = team_size  # public
+
+    def show_details(self):
+        print(f"Name: {self.name}")                        # public
+        print(f"Department: {self._department}")           # protected
+        print(f"Salary: {self.show_salary()}")             # private (via method)
+        print(f"Team Size: {self.team_size}")
+
+mgr = Manager("Aniruddhan", 85000, "AI Research", 5)
+mgr.show_details()
+
